0294e8ab1ee04b1ed572f8498cfbde51a5a84862
Subash Venkat <subash.venkat2023@vitstudent.ac.in>
2025-03-23 20:25:41 +0530

    Refactor login functionality and enhance password confirmation validation

--- src/login.js
+++ src/login.js
@@ -4,6 +4,8 @@ document.getElementById('signupTab').addEventListener('click', function (e) {
     document.getElementById('signupFormContainer').classList.remove('hidden');
     document.getElementById('loginTab').classList.remove('active');
     document.getElementById('signupTab').classList.add('active');
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
   });
   
   document.getElementById('loginTab').addEventListener('click', function (e) {
@@ -12,6 +14,11 @@ document.getElementById('signupTab').addEventListener('click', function (e) {
     document.getElementById('loginFormContainer').classList.remove('hidden');
     document.getElementById('signupTab').classList.remove('active');
     document.getElementById('loginTab').classList.add('active');
+    document.getElementById('newUsername').value = '';
+    document.getElementById('newPassword').value = '';
+    document.getElementById('confirmPassword').value = '';
+    document.getElementById('confirmPassword').style.border = '';
+      document.getElementById('confirmPassword').style.color = '';
   });
 
   function loginUser(e){
@@ -30,3 +37,20 @@ document.getElementById('signupTab').addEventListener('click', function (e) {
 
   document.getElementById('loginForm').addEventListener('submit', loginUser);
   document.getElementById('signupForm').addEventListener('submit', signupUser);
+
+  function checkPassword(){
+    const password = document.getElementById('newPassword').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    if(password.length === 0 || confirmPassword.length === 0){
+      document.getElementById('confirmPassword').style.border = '';
+      document.getElementById('confirmPassword').style.color = '';
+      return;
+    }
+    if(password !== confirmPassword){
+      document.getElementById('confirmPassword').style.border = '1px solid red';
+      document.getElementById('confirmPassword').style.color = 'red';
+    } else {
+      document.getElementById('confirmPassword').style.border = '';
+      document.getElementById('confirmPassword').style.color = '';
+    }
+  }
