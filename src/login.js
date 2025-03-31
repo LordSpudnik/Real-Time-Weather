89430d095fb4dea9390bffd2fc3a9cf467613c96
Subash Venkat <subash.venkat2023@vitstudent.ac.in>
2025-03-31 18:56:09 +0530

    made changes to base-url for deployment

--- src/login.js
+++ src/login.js
@@ -32,7 +32,7 @@ async function loginUser(e) {
     }
 
     try {
-        let response = await fetch('http://localhost:5000/api/login', {
+        let response = await fetch('https://rt-weather.onrender.com/api/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -71,7 +71,7 @@ async function signupUser(e) {
     }
 
     try {
-        let response = await fetch('http://localhost:5000/api/signup', {
+        let response = await fetch('https://rt-weather.onrender.com/api/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
